fe8025bbd8c17f743538539b2f9deae926876847
abdullahbyrmv <abdullah.bayramov@inbox.ru>
2022-08-30 12:34:05 +0400

    LinkedList

---
+++ src/Singly_Linked_List/Main.java
@@ -0,0 +1,10 @@
+package Singly_Linked_List;
+
+public class Main {
+    public static void main(String[] args) {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.createSinglyLinkedList(5);
+        list.insertIntoLinkedList(1,1);
+        System.out.println(list.searchNode(1));
+    }
+}
---
+++ src/Singly_Linked_List/Node.java
@@ -0,0 +1,6 @@
+package Singly_Linked_List;
+
+public class Node {
+    public int value;
+    public Node next;
+}
